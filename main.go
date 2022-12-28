52ddb4d2d2c1f006b6fa3f753e7d1da82164a78a
csn2002 <csnaruka76@gmail.com>
2022-12-28 20:11:26 +0530

    Added some more functionalities in main go

--- main.go
+++ main.go
@@ -5,13 +5,23 @@ import (
 	"net/http"
 )
 
-// hame handler for executing home page logic
+// Added some more functionalities as handler
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
+}
+func showsnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet"))
+}
+func createsnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("create a new snippet"))
 }
 func main() {
 	mux := http.NewServeMux() //new serve mux used to map the url pattern ("/") to the handler home
 	mux.HandleFunc("/", home)
+	//"/snippet","/snippet/create" are fixed path patterns while "/" followed a subtree patterns(it's a catch-all)
+	mux.HandleFunc("/snippet", showsnippet)
+	mux.HandleFunc("/snippet/create", createsnippet)
+
 	//listen and serve is used to start a web server with two parameter "TCP network address", servemux
 	//log is used to log error if it returns
 	log.Println("Starting server on :4000")
